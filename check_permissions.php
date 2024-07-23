a62e346ba9d3e74fc4d1f88290fd84499a834eb8
Martin Gausseran <martin.gausseran@gmail.com>
2024-07-23 15:34:05 +0200

    Suppression du fichier check_permissions.php

--- check_permissions.php
+++
@@ -1,9 +0,0 @@
-<?php
-$file_path = 'submissions.txt';
-
-if (is_writable($file_path)) {
-    echo "Le fichier est accessible en écriture.";
-} else {
-    echo "Le fichier n'est pas accessible en écriture.";
-}
-?>
