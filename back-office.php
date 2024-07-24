fe7923fb5c44431861a886cb078585985bb649e5
MGausseran <martin.gausseran@gmail.com>
2024-07-24 11:57:50 +0200

    Les données s'inscrivent dans le fichier submissions, et la page de confirmation d'envoi s'affiche après validation du formulaire.

--- back-office.php
+++ back-office.php
@@ -5,10 +5,11 @@ if (isset($_POST["submit"])) {
     $motif = $_POST["motif"];
     $message = $_POST["message"];
     //Les différentes informations renseignées dans le formulaire sont stockées dans ces 4 variables.
-    echo "<p>Les données reçues sont les suivantes:<br>Nom et prénom :$name<br>Email : $email<br>Motif : $motif<br>Message : $message</p>";
     $file = fopen('submissions.txt', 'a');
     fwrite($file, "Name: $name\nEmail: $email\nMotif: $motif\nMessage: $message\n\n");
     fclose($file);
     //On 'append' dans le fichier 'submissions.txt' les différentes informations renseignées plus haut.
+    header("Location: contact-confirmed.html");
+    exit();
 }
 ?>
